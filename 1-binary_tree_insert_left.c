b38086f4b5432c332948126a45a442334feeab93
JuanManuelReyes <manu_reyes2003@hotmail.com>
2022-07-15 10:49:05 -0300

    Task 2

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -38,7 +38,7 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 	if (parent == NULL)
 		return (NULL);
 	
-	left_node = binary_tree_node(parent, value)
+	left_node = binary_tree_node(parent, value);
 
 	if (left_node == NULL)
 		return (NULL);
