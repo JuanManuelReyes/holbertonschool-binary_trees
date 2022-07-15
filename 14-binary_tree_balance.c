cf6d38f0ed73a5ebd64f547a15c425c54b7d294b
JuanManuelReyes <manu_reyes2003@hotmail.com>
2022-07-15 14:09:29 -0300

    Task 15 Fixed

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -24,7 +24,7 @@ size_t binary_tree_height(const binary_tree_t *tree)
 {
         size_t right_side = 0, left_side = 0;
 
-        if (tree == NULL || binary_tree_is_leaf(tree))
+        if (tree == NULL)
                 return (0);
         else
         {
@@ -51,8 +51,8 @@ int binary_tree_balance(const binary_tree_t *tree)
         if (tree == NULL)
                 return (0);
         
-        left_side = binary_tree_height(tree->left);
         right_side = binary_tree_height(tree->right);
+        left_side = binary_tree_height(tree->left);
 
-        return (left_side - right_side);
+        return (right_side - left_side);
 }
