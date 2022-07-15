43b75dee41089190d0514521796efe3ba7854ce6
JuanManuelReyes <manu_reyes2003@hotmail.com>
2022-07-15 11:29:46 -0300

    Task 6

--- 5-binary_tree_is_root.c
+++ 5-binary_tree_is_root.c
@@ -8,4 +8,8 @@
  **/
 int binary_tree_is_root(const binary_tree_t *node)
 {
+        if (node != NULL && node->left != NULL && node->right != NULL)
+                return (1);
+        
+        return (0);
 }
