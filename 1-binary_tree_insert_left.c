9f4597ace6f98a99308aefac86b2f720c3de2329
JuanManuelReyes <manu_reyes2003@hotmail.com>
2022-07-15 11:00:48 -0300

    Task 2 Fixed

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -18,11 +18,13 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 
 	if (left_node == NULL)
 		return (NULL);
+
+	left_node->left = parent->left;
 	
-	if (parent->left == NULL)
-		parent->left = left_node;
+	if (parent->left != NULL)
+		left_node->left->parent = left_node;
 	
-	left_node->left->parent = left_node;
+	parent->left = left_node;
 
 	return (left_node);
 }
