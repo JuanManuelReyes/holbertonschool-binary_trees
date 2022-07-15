7c6365c151662dbcf3fecffb84d3d433d618b1fb
JuanManuelReyes <manu_reyes2003@hotmail.com>
2022-07-15 12:19:06 -0300

    Task 11

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -8,4 +8,18 @@
  **/
 size_t binary_tree_depth(const binary_tree_t *tree)
 {
+        size_t right_side = 0, left_side = 0;
+
+        if (tree == NULL)
+                return (0);
+        else
+        {
+                left_side = binary_tree_depth(tree->left);
+                right_side = binary_tree_depth(tree->right);
+
+                if (left_side > right_side)
+                        return (left_side + 1);
+                else
+                        return (right_side + 1);
+        }
 }
