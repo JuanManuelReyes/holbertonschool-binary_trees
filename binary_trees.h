80586e5351ad4d3a5797047b2b0fc3977f750901
JuanManuelReyes <manu_reyes2003@hotmail.com>
2022-07-15 10:00:23 -0300

    Binary Print Added

--- binary_trees.h
+++ binary_trees.h
@@ -66,4 +66,7 @@ heap_t *array_to_heap(int *array, size_t size);
 int heap_extract(heap_t **root);
 int *heap_to_sorted_array(heap_t *heap, size_t *size);
 
+/*Print Tree*/
+void binary_tree_print(const binary_tree_t *);
+
 #endif 
