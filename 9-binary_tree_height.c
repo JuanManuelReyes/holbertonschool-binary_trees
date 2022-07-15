c065ec22a56ca1eccee1412d1b58585e780fd5b9
JuanManuelReyes <manu_reyes2003@hotmail.com>
2022-07-15 12:01:26 -0300

    Task 10 Fixed

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -10,11 +10,11 @@ size_t binary_tree_height(const binary_tree_t *tree)
 {
         size_t right_side = 0, left_side = 0;
 
-        if (tree == NULL || )
+        if (tree == NULL)
                 return (0);
         
         left_side = binary_tree_height(tree->left);
         right_side = binary_tree_height(tree->right);
 
-        return max(left_side, right_side) + 1;
+        return Max(left_side, right_side) + 1;
 }
