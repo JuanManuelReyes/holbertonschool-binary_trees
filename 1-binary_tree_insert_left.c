80bad1c9c738a67353265196bee9a7e5bb1cf61e
MateoArbini <mateoarbini1998@gmail.com>
2022-07-16 14:18:00 -0700

    tasks OK with Betty OK

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -13,17 +13,17 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 
 	if (parent == NULL)
 		return (NULL);
-	
+
 	left_node = binary_tree_node(parent, value);
 
 	if (left_node == NULL)
 		return (NULL);
 
 	left_node->left = parent->left;
-	
+
 	if (parent->left != NULL)
 		left_node->left->parent = left_node;
-	
+
 	parent->left = left_node;
 
 	return (left_node);
